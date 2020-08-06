4d2d15df84a5141ac2029dc03d250ffe0e5f27c0
tropek <67675479+tropek@users.noreply.github.com>
2020-08-06 03:40:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-﻿const Discord = require("discord.js");
+const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "$";
 
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Brodcast Bot ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$bc |DF_Team`,"http://twitch.tv/Death Shop")
+client.user.setGame(`$bc | F5mah`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
